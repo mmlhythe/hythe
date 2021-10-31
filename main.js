0ee6ec64ddde9395c6265ee1afbee5d092b4091b
Trumboan <zvothsven@gmail.com>
2021-10-31 12:09:35 -0700

    changes

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("Hello world !!!");
+
+function printamessage() {
+    alert("My name is Bond");
+}
+
+var password;
+var password1=("000");
+password = prompt("Please enter password here");
+if (password === password1){
+    alert("correct password! Click \"OK\" to enter")
+}
+else{
+    window.location = "https://google.com";
+}
